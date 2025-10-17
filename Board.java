753d6a361dc7c1795f2b67754bb567d1da385585
vijsak27 <146001383+vijsak27@users.noreply.github.com>
2025-10-17 02:41:38 +0000

    updated citations and comments

--- Board.java
+++ Board.java
@@ -1,11 +1,11 @@
 // Make the board class that has the toString method
-
+// used runestone 1.15 and 3.6
 public class Board{
     String board[][]= new String[10][10];
     public Board() { // define a standard board as empty
         for (int i = 0; i<board.length;i++ ){
             for (int n = 0; n<board[i].length; n++){
-                board[i][n]= " - ";//loop through and fill with '-'
+                board[i][n]= " - ";//loop through and fill with " - "
                 
             }
 
--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main{
     public static void main(String[] args){
-        Board b1 = new Board();
+        Board b1 = new Board();// make two boards
         Board b2 = new Board();
-        System.out.println(b1);
+        System.out.println(b1);// print both boards
         System.out.println(b2);
 
     }
