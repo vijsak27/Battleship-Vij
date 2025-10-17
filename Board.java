cda0c0a617d791cb41bc722858cb346930643955
Vij <vijsak27@7hills.org>
2025-10-17 09:36:24 -0400

    continyed workng on board.java. wokring o check placmet an dplaceships

--- Board.java
+++ Board.java
@@ -3,6 +3,7 @@
 import java.util.Scanner;
 public class Board{
 	private String[] shipNames = {"Cruiser (Length 5)", "Battleship (Length 4)", "Destroyer (Length 3)", "Submarine (Length 3)", "Patrol (Length 2)"};
+	private int[] shipLengths = {5,4,3,3,2};
 	Scanner s1 = new Scanner (System.in);
     String board[][]= new String[10][10];
     public Board() { // define a standard board as empty
@@ -50,20 +51,28 @@ public class Board{
 
         }
 
-    }
-
+    
+	public boolean checkPlacement(int[] coords,int shipNumber){
+		int shipLength = shipLengths[shipNumber];
+		//figure out how to check vlidity of placement
+		}
     public void placeShips(Board b){
-        for(int i = 0; i)
-			System.out.println("Placing ships\n");
+        for(int i = 0; i<shipNames.length; i++){
 			b.clearBoard();
-			System.out.println("Place Cruiser (length 5) \n\n Where would you like to place the Cruiser \n Row Letter (A-J): ");
+			System.out.println("Place " + shipNames[i]+ "\n\n Where would you like to place the "+shipNames[i]" \nRow Letter (A-J): ");
 			String row = scan.nextLine();
 			System.out.println("Column (Integer from 1-10): ");
 			int column = (scan.nextInt()-1);
 			String temp = scan.nextLine();
+			System.out.println("Horizontal (H) or Vertical (V): ");
+			// work here to get horizonatl and verical
 			int[] coords = b.convertCoords(row,column);
+			if (checkPlacement)
+			else{
+			 i--;	
+			}
 		
-        
+	}
         
 
     }
